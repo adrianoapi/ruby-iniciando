6b26570adeb9eec8673ee2bc26e873b4567b97a9
Adriano A Costa <sdcomputadores@gmail.com>
2017-01-20 13:29:32 -0200

    iniciando projeto

---
+++ hello_world.rb
@@ -0,0 +1,10 @@
+#puts "Heloo World!"
+#print "Heloo World!"
+#printf("Heloo World!")
+
+puts 'Qual seu nome?'
+$name = STDIN.gets
+puts  'Hello world ' + $name
+# var //local 
+# @var //instance 
+# $var //global 
