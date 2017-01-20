a1ab4c71c730624ff583c034b66c638c58e0110c
Adriano A Costa <sdcomputadores@gmail.com>
2017-01-20 14:02:42 -0200

    operadores de comparação

---
+++ conditions.rb
@@ -0,0 +1,10 @@
+number1 = 1
+number2 = 0
+total = 0
+
+#puts number1 == number2
+#puts number1 != number2
+#puts number1.equal? number2
+#puts number1.eql? number2
+#puts number1 > number2
+puts number1 <=> number2
